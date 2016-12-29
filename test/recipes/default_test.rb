dd27aa86ad69699299a705bb6daf68cb886f8e62
dmlb2000 <dmlb2000@gmail.com>
2016-12-28 16:14:09 -0800

    fix code style bits

--- test/recipes/default_test.rb
+++ test/recipes/default_test.rb
@@ -6,13 +6,13 @@
 # found at https://docs.chef.io/inspec_reference.html
 
 %w(
-8080
-8081
-8066
-8121
-8181
-8051
-80
+  8080
+  8081
+  8066
+  8121
+  8181
+  8051
+  80
 ).each do |port|
   describe port(port.to_i) do
     it { should be_listening }
