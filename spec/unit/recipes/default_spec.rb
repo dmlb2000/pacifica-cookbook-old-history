25b676b01ccfcd02913712557867696abfee5a62
dmlb2000 <dmlb2000@gmail.com>
2016-12-29 14:26:05 -0800

    fix this bit

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -48,7 +48,7 @@ describe 'test::pacifica' do
   context 'When all attributes are default, on an unspecified platform' do
     let(:chef_run) do
       runner = ChefSpec::ServerRunner.new(
-       platform: 'centos', version: '7.2.1511'
+        platform: 'centos', version: '7.2.1511'
       )
       runner.converge(described_recipe)
     end
