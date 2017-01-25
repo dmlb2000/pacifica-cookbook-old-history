c28914a73d5d95238ee6604368d5c89851a7715a
I Smith <ian.smith@pnnl.gov>
2017-01-24 17:13:13 -0800

    Moving version to normal location

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,13 @@ maintainer_email 'dmlb2000@gmail.com'
 license 'Apache v2.0'
 description 'Installs/Configures pacifica'
 long_description 'Installs/Configures pacifica'
-version '0.1.0'
 if respond_to?(:issues_url)
   issues_url 'https://github.com/pacifica/pacifica-cookbook/issues'
 end
 if respond_to?(:source_url)
   source_url 'https://github.com/pacifica/pacifica-cookbook'
 end
+version '0.1.0'
 
 depends 'git'
 depends 'poise-python'
