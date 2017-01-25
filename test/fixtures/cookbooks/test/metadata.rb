5e66cc91ac7dbc9649a1b799622a93732836f649
I Smith <ian.smith@pnnl.gov>
2017-01-24 17:14:36 -0800

    Syncing up with the long_description of the main metadata.rb file

--- test/fixtures/cookbooks/test/metadata.rb
+++ test/fixtures/cookbooks/test/metadata.rb
@@ -3,7 +3,7 @@ maintainer 'David Brown'
 maintainer_email 'dmlb2000@gmail.com'
 license 'Apache v2.0'
 description 'Installs/Configures pacifica'
-long_description 'Installs/Configures pacifica'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 if respond_to?(:issues_url)
   issues_url 'https://github.com/pacifica/pacifica-cookbook/issues'
 end
