2f62e1c328c2bc1859e3d8009688cfb92e1b5aa5
I Smith <ian.smith@pnnl.gov>
2017-01-24 17:17:16 -0800

    Adding matchers for all the custom resources.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,7 +1,73 @@
 if defined?(ChefSpec)
-  def create_pacifica_archiveinterface(name)
+  def create_pacifica_archiveinterface(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(
-      :pacifica_archiveinterface, :create, name
+      :pacifica_archiveinterface, :create, resource_name
+    )
+  end
+
+  def create_pacifica_cartbackend(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_cartbackend, :create, resource_name
+    )
+  end
+
+  def create_pacifica_cartfrontend(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_cartfrontend, :create, resource_name
+    )
+  end
+
+  def create_pacifica_ingestbackend(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_archiveinterface, :create, resource_name
+    )
+  end
+
+  def create_pacifica_ingestfrontend(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_ingestfrontend, :create, resource_name
+    )
+  end
+
+  def create_pacifica_metadata(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_metadata, :create, resource_name
+    )
+  end
+
+  def create_pacifica_nginx(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_nginx, :create, resource_name
+    )
+  end
+
+  def create_pacifica_policy(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_policy, :create, resource_name
+    )
+  end
+
+  def create_pacifica_reporting(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_reporting, :create, resource_name
+    )
+  end
+
+  def create_pacifica_status(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_status, :create, resource_name
+    )
+  end
+
+  def create_pacifica_uniqueid(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_uniqueid, :create, resource_name
+    )
+  end
+
+  def create_pacifica_varnish(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(
+      :pacifica_varnish, :create, resource_name
     )
   end
 end
