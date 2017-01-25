6a62b53b3442e3671f6d918439ad83844a8eb66a
I Smith <ian.smith@pnnl.gov>
2017-01-24 17:17:00 -0800

    Adding the supported platform feature, more verbose rspec output, including the library resource files to be used in rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,12 @@
 require 'chefspec'
 require 'chefspec/berkshelf'
+
+Dir['./spec/support/**/*.rb'].sort.each { |f| require f }
+Dir['libraries/*.rb'].each { |f| require File.expand_path(f) }
+
+RSpec.configure do |config|
+  config.formatter = :documentation
+  config.color = true
+end
+
+at_exit { ChefSpec::Coverage.report! }
