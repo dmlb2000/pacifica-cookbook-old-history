f0823b6c7ccc2b22e41bde6bf3e8919b8990959b
I Smith <ian.smith@pnnl.gov>
2017-01-25 21:21:12 -0800

    Some things in this were claimed to be nil per chefspec, removed and things started working
    I don’t see where this is being used anyplace, I can put it back if we need to..

--- test/fixtures/cookbooks/test/attributes/default.rb
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -1,4 +1,3 @@
 default['postgresql']['password']['postgres'] = 'postgres'
 default['java']['jdk_version'] = '8'
 default['elasticsearch']['install']['version'] = '5.1.2'
-default['ohai']['plugin_path'] = ::File.join(::File.dirname(Chef::Config['config_file']), 'ohai', 'plugins')
