4694fd575ef78dc52fdb5c6de6cbc9c0aa81f391
I Smith <ian.smith@pnnl.gov>
2017-01-25 18:08:52 -0800

    reordering a tad

--- spec/support/supported_platforms.rb
+++ spec/support/supported_platforms.rb
@@ -1,7 +1,7 @@
 def supported_platforms
   _platforms = {
-    'ubuntu' => ['16.04'],
+    'centos' => ['7.2.1511'],
     'redhat' => ['7.1'],
-    'centos' => ['7.2.1511']
+    'ubuntu' => ['16.04']
   }
 end
