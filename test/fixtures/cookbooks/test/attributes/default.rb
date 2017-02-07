9aa9d5ff20166c30cd950ffd1ca00c421c5269f3
I Smith <ian.smith@pnnl.gov>
2017-02-07 10:37:53 -0800

    addressing the versioning issue in the ubuntu deployment, no 5.1.2 version seems to be available according to tests
    We can further verify this

--- test/fixtures/cookbooks/test/attributes/default.rb
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -1,3 +1,8 @@
 default['postgresql']['password']['postgres'] = 'postgres'
 default['java']['jdk_version'] = '8'
-default['elasticsearch']['install']['version'] = '5.1.2'
+case node['platform_family']
+when 'rhel'
+  override['elasticsearch']['install']['version'] = '5.1.2'
+when 'debian'
+  default['elasticsearch']['install']['version'] = '5.2.0'
+end
