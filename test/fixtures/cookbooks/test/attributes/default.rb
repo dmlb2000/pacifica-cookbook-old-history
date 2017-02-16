b73d0d248ce80db85616781fa79c45872cbb1698
David Brown <dmlb2000@gmail.com>
2017-02-16 07:55:06 -0800

    fix some elasticsearch versions and vhost options

--- test/fixtures/cookbooks/test/attributes/default.rb
+++ test/fixtures/cookbooks/test/attributes/default.rb
@@ -2,20 +2,20 @@ default['postgresql']['password']['postgres'] = 'postgres'
 default['java']['jdk_version'] = '8'
 case node['platform_family']
 when 'rhel'
-  override['elasticsearch']['install']['version'] = '5.1.2'
+  override['elasticsearch']['install']['version'] = '5.2.1'
 when 'debian'
   default['elasticsearch']['install']['version'] = '5.2.0'
 end
 
 default['rabbitmq']['loopback_users'] = []
-default['rabbitmq']['virtualhosts'] = %w(/cart /ingest /Uploader)
+default['rabbitmq']['virtualhosts'] = %w(/cart /ingest /uploader)
 default['rabbitmq']['enabled_users'] = [
   {
     name: 'guest',
     password: 'guest',
     rights: [
       {
-        vhost: '/Uploader',
+        vhost: '/uploader',
         conf: '.*',
         write: '.*',
         read: '.*',
